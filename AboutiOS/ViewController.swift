536c177df8313f0170106fd11c36f4895f6206bf
zongbeen Han <hjb825@naver.com>
2025-03-07 21:38:01 +0900

    Initial commit

---
+++ AboutiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutiOS
+//
+//  Created by 한종빈 on 3/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
