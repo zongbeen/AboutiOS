2df937d0e9ae1d7c47709a98ab0c7bc9e630dd8d
zongbeen Han <hjb825@naver.com>
2025-03-24 22:47:35 +0900

    Update project.pbxproj

--- AboutiOS/Model/Section.swift
+++ AboutiOS/Model/Section.swift
@@ -5,13 +5,13 @@
 //  Created by 한종빈 on 3/15/25.
 //
 
-struct Section {
+struct Section: Codable {
     let header: String?
     let footer: String?
-    var items: [Item]
+    var items: [Item] // var 사용 가능
 }
 
-struct Item {
+struct Item: Codable {
     let imageName: String
     let title: String
 }
