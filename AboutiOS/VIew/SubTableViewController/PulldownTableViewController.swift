9c929b0d39c6f8275522ef6cd67fed219bd77e8b
zongbeen Han <hjb825@naver.com>
2025-03-30 16:13:32 +0900

    Add PulldownTableViewController

---
+++ AboutiOS/VIew/SubTableViewController/PulldownTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PulldownTableViewController.swift
+//  AboutiOS
+//
+//  Created by 한종빈 on 3/30/25.
+//
+
+import UIKit
+
+class PulldownTableViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
