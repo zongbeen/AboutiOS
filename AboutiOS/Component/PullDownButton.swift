38f7f5fe11c10f802aed3714cf0bb954302843eb
zongbeen Han <hjb825@naver.com>
2025-04-03 21:00:15 +0900

    PullDownButton 주석추가

--- AboutiOS/Component/PullDownButton.swift
+++ AboutiOS/Component/PullDownButton.swift
@@ -13,15 +13,22 @@ class PullDownButton<T>: UIButton {
     
     init(configModel: PullDownOption, keyPath: WritableKeyPath<PullDownOption, T>, options: [T]) {
         super.init(frame: .zero)
+        //configModel과 keyPath를 저장
         self.configModel = configModel
         self.keyPath = keyPath
+        
+        //버튼을 탭하면 바로 메뉴 표시
         self.showsMenuAsPrimaryAction = true
         self.tintColor = .systemGray
+        
+        //텍스트를 오른쪽 정렬
         self.contentHorizontalAlignment = .trailing
         self.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
         
+        //초기 버튼 타이틀 설정
         updateTitle(with: configModel[keyPath: keyPath])
         
+        //UIMenu를 생성하여 드롭다운 메뉴 구성
         self.menu = UIMenu(children: options.map { option in
             UIAction(title: "\(option)") { [weak self] _ in
                 guard let self = self else { return }
@@ -30,6 +37,7 @@ class PullDownButton<T>: UIButton {
             }
         })
         
+        //버튼 크기 조정 - 글자 수에 따른 크기 변경
         self.sizeToFit()
         self.frame.size = CGSize(width: max(self.frame.width, 80), height: max(self.frame.height, 30))
     }
@@ -40,6 +48,7 @@ class PullDownButton<T>: UIButton {
     
     private func updateTitle(with value: T) {
         let image = UIImage(systemName: "chevron.up.chevron.down")!
+        //텍스트 + 아이콘
         let attributedString = NSMutableAttributedString(string: "\(value) ")
         let attachment = NSTextAttachment()
         attachment.image = image.withRenderingMode(.alwaysTemplate)
